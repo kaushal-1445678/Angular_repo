9d181f7c8ee79877b693aac861064066097b0c23
kaushal-1445678 <kaushal730223@gmail.com>
2026-05-10 15:57:40 +0000

    update mis correction

--- day10/todo-app/src/app/user.component.ts
+++ day10/todo-app/src/app/user.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [ RouterModule, RouterLink],
   template: `
     <h1>User Details</h1>
     <p>User ID: {{ userId }}</p>
     <a routerLink="/">Back to Home</a>
+    <router-outlet></router-outlet>
   `,
   styles: [`
     h1 { color: purple; }
